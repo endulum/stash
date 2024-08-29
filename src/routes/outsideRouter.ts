7604f4ddba3c5084aa90f3e23b281fd1159ed04a
endulum <71853916+endulum@users.noreply.github.com>
2024-08-29 00:19:30 -0700

    add middleware for login

--- src/routes/outsideRouter.ts
+++ src/routes/outsideRouter.ts
@@ -1,5 +1,7 @@
-import express, {type Response} from 'express';
+import express, { type Response } from 'express';
 import asyncHandler from 'express-async-handler';
+import { body, validationResult } from 'express-validator';
+import passport from 'passport';
 
 const router = express.Router();
 
@@ -7,10 +9,46 @@ const renderLogin = asyncHandler(async (req, res, next) => {
   return res.render('outside', {
     page: 'outside/login',
     title: 'Log In',
-    prevForm: req.prevForm,
+    prevForm: req.body,
     formErrors: req.formErrors,
     formMessage: req.formMessage
   })
+})
+
+const validateLoginForm = [
+  body('username')
+    .trim()
+    .notEmpty().withMessage('Please enter a username.')
+    .escape(),
+  body('password')
+    .notEmpty().withMessage('Please enter a password.')
+    .trim().escape()
+]
+
+const handleValidationErrors = asyncHandler(async (req, res, next) => {
+  const errorsArray = validationResult(req).array()
+  if (errorsArray.length > 0) {
+    req.formErrors = {}
+    errorsArray.forEach(error => {
+      if (req.formErrors && error.type === 'field') 
+        req.formErrors[error.path] = error.msg
+    })
+  }
+  return next()
+})
+
+const handleLoginForm = asyncHandler(async (req, res, next) => {
+  if (req.formErrors) return renderLogin(req, res, next)
+  passport.authenticate('local', (err: Error, user: Express.User) => {
+    if (err) return next(err)
+    if (!user) {
+      req.formErrors = { password: 'Incorrect username or password.' }
+      return renderLogin(req, res, next)
+    } else req.logIn(user, (err) => {
+      if (err) return next(err)
+      return res.redirect('/')
+    })
+  })(req, res, next)
 })
 
 const renderSignup = asyncHandler(async (req, res, next) => {
@@ -25,7 +63,7 @@ const renderSignup = asyncHandler(async (req, res, next) => {
 
 router.route('/login')
   .get(renderLogin)
-//   .post(validateLoginForm, handleLoginForm)
+  .post(validateLoginForm, handleValidationErrors, handleLoginForm)
 router.route('/signup')
   .get(renderSignup)
 //   .post(validateSignupForm, handleSignupForm)
