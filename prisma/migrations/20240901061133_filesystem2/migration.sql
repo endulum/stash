2c6bc41d25fa43d6d224008f45dd82c36b2a579c
endulum <71853916+endulum@users.noreply.github.com>
2024-08-31 23:13:22 -0700

    draft filesystem schema

---
+++ prisma/migrations/20240901061133_filesystem2/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - Added the required column `size` to the `File` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `File` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "File" DROP CONSTRAINT "File_folderId_fkey";
+
+-- AlterTable
+ALTER TABLE "File" ADD COLUMN     "size" DOUBLE PRECISION NOT NULL,
+ADD COLUMN     "type" TEXT NOT NULL,
+ALTER COLUMN "folderId" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
