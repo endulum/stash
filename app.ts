0639fcfdd5c85692f7905c313237c4d0fd4a52cf
endulum <71853916+endulum@users.noreply.github.com>
2025-01-16 20:48:01 -0800

    update app.ts entrypoint and env var structure
    - Cut off app.ts from rest of app, to start at beginning and work own way up.
    - Add Helmet for security, plus some other security tweaks
    - Add `envinit` script to easily create the three required env files,
      with the same structure
    - Add Morgan for logging
    - Add compression
    - Tighten cookie security for production
    - Nicer "server start" logging showing port and mode

--- app.ts
+++ app.ts
@@ -1,66 +1,84 @@
-import dotenv from 'dotenv';
+import express from "express";
+import path from "path";
+import session from "express-session";
+import passport from "passport";
+import compression from "compression";
+import flash from "connect-flash";
+import helmet from "helmet";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
+import logger from "morgan";
+import dotenv from "dotenv";
+import "./config/passport";
 
-dotenv.config({ path: '.env.' + process.env.ENV })
+dotenv.config({ path: ".env." + process.env.NODE_ENV });
 
-import './config/passport';
-import path from 'path';
-import express from 'express';
-import flash from 'connect-flash';
-import asyncHandler from 'express-async-handler';
-import session from 'express-session';
-import passport from 'passport';
-import { PrismaSessionStore } from '@quixo3/prisma-session-store';
-import { PrismaClient } from '@prisma/client';
+const app = express();
 
-import errorHandler from './src/middleware/errorHandler'
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "src/public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.disable("x-powered-by");
+app.set("trust proxy", 1);
+app.use(compression());
+app.use(flash());
 
-import authRouter from './src/routes/auth';
-import mainRouter from './src/routes/main';
-import shareRouter from './src/routes/share'
+app.use(
+  helmet({
+    ...(process.env.SUPABASE_URL && {
+      contentSecurityPolicy: {
+        directives: {
+          "img-src": ["'self'", process.env.SUPABASE_URL],
+        },
+      },
+    }),
+  })
+);
 
-const secret: string | undefined = process.env.SECRET
-if (secret === undefined) throw new Error('Secret is not defined.')
-
-const app = express()
-
-app.set('views', path.join(__dirname, 'src/views'))
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'src/public')))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(session({
-  secret,
-  resave: false,
-  saveUninitialized: true,
-  store: new PrismaSessionStore(
-    new PrismaClient(),
-    {
+app.use(
+  session({
+    name: "sessionId",
+    secret:
+      process.env.SESSION_SECRET ||
+      (() => {
+        throw new Error("Session secret is not defined.");
+      })(),
+    resave: false,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
       checkPeriod: 2 * 60 * 1000,
       dbRecordIdIsSessionId: true,
-      dbRecordIdFunction: undefined
-    }
-  ),
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24
-  }
-}))
-app.use(flash())
-app.use(passport.initialize())
-app.use(passport.session())
+      dbRecordIdFunction: undefined,
+    }),
+    cookie: {
+      ...(process.env.NODE_ENV === "production" && {
+        secure: true,
+        httpOnly: true,
+      }),
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(asyncHandler(async (req, res, next) => {
-  res.locals.user = req.user
-  res.locals.warning = req.flash('warning')
-  res.locals.success = req.flash('success')
-  return next()
-}))
+if (process.env.NODE_ENV === "development") {
+  app.use(logger("dev"));
+}
 
-app.use('/share', shareRouter)
+app.get("/", async (req, res) => {
+  res.sendStatus(200);
+});
 
-app.use(asyncHandler(async (req, res, next) => {
-  return req.user ? mainRouter(req, res, next) : authRouter(req, res, next)
-}))
+app.get("*", async (req, res) => {
+  res.sendStatus(404);
+});
 
-app.use(errorHandler)
-
-app.listen(process.env.PORT ?? 3000)
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  process.stdout.write(
+    `⚡️ server starting at http://localhost:${port} in ${process.env.NODE_ENV} mode\n`
+  );
+});
