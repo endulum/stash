8962e5359a89ba367d19caf83d2920751018dfcd
endulum <71853916+endulum@users.noreply.github.com>
2025-01-29 19:59:51 -0800

    get rid of unnecessary logSession middleware

--- src/middleware/logSession.ts
+++
@@ -1,9 +0,0 @@
-import asyncHandler = require("express-async-handler");
-
-const logSession = asyncHandler(async (req, res, next) => {
-  console.log(req.session || '(no session)');
-  console.log(req.user || '(no user)');
-  return next();
-})
-
-export default logSession;
